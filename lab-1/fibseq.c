2267d75ff55c7aa7784fbf5e82994daa9591b506
Koding <hylin.embedded.2015@gmail.com>
2015-07-23 08:19:50 +0000

    Added time measurement to lab-1

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
+    int number=0;
+    int result=0;
+    
+    clock_t begin, end;
+    double time_spent;
+    
+    scanf("%d", &number);
+    
+    begin = clock();
+    result = fibonacci(number);   
+    end = clock();
+    
+    time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
+    printf("The fibonacci sequence at %d is: %d , (%f sec) \n", number, result, time_spent);
 
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+    return(0);
 }
-
